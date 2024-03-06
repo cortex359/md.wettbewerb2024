61538393ec4a197a0382d63219ada46a22682c7f
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2024-03-06 21:54:55 +0100

    Score 25318.381  (Overlap:   4.8950, Distance:  18.1088, Angle:  12.1178, n: 133, k: 554) result_files/Area_Afro-Eurasia_score_25318.381279.txt Score 21588.808  (Overlap:   0.3704, Distance:   6.1664, Angle:   5.6181, n:  23, k:  74) result_files/Area_Americas_score_21588.807556.txt Score 22526.243  (Overlap:   0.3097, Distance:   7.2939, Angle:  10.1632, n:  43, k: 156) result_files/Area_Asia_score_22526.242705.txt Score 19223.598  (Overlap:   0.2342, Distance:   7.1251, Angle:   7.2034, n:  24, k:  94) result_files/Area_Europe_score_19223.598045.txt Score  7019.675  (Overlap:   8.9236, Distance:  38.5811, Angle:  15.0636, n: 127, k: 538) result_files/CO2_Production_Afro-Eurasia_score_7019.675211.txt Score  4757.593  (Overlap:   0.6358, Distance:   4.7933, Angle:   8.3498, n:  10, k:  17) result_files/Deutschlands_Nachbarn_score_4757.593062.txt Score  8234.438  (Overlap:   7.8700, Distance:  34.6170, Angle:  16.1497, n: 126, k: 534) result_files/GNI_per_capita_Afro-Eurasia_score_8234.437923.txt Score 10964.819  (Overlap:   0.3186, Distance:   6.7953, Angle:  10.6863, n:  27, k:  72) result_files/Instant_Noodle_Consumption_Eurasia_score_10964.819433.txt Score 13269.516  (Overlap:   4.3196, Distance:  25.9740, Angle:  17.4209, n: 133, k: 554) result_files/Population_Afro-Eurasia_score_13269.515812.txt Score 23098.247  (Overlap:   0.3020, Distance:   5.4212, Angle:   5.8666, n:  23, k:  74) result_files/Population_Americas_score_23098.246774.txt Score  4119.183  (Overlap:  13.5707, Distance:  49.4337, Angle:  22.4408, n: 133, k: 554) result_files/Population_Density_Afro-Eurasia_score_4119.183218.txt Score  6321.935  (Overlap:   2.4741, Distance:  11.9766, Angle:  11.4537, n:  23, k:  74) result_files/Population_Density_Americas_score_6321.934566.txt → 166,442.44

---
+++ slurm/run_07_PAE.zsh
@@ -0,0 +1,46 @@
+#!/usr/bin/zsh
+
+### Start of Slurm SBATCH definitions
+# Ask for eight tasks (same as 8 cores for this example)
+#SBATCH --ntasks=12
+#SBATCH --array=1-12
+### Ask for one node, use several nodes in case you need additional resources
+#SBATCH --nodes=12
+# Ask for the maximum memory per core (most CLAIX-2023 nodes: 2.7GB per cpu)
+#SBATCH --mem-per-cpu=40M
+#SBATCH --time=08:05:00
+# Name the job
+#SBATCH --job-name=run_07_8h
+# Declare a file where the STDOUT/STDERR outputs will be written
+#SBATCH --output=output.%A_%a.txt
+### end of Slurm SBATCH definitions
+
+### your program goes here
+# `srun` runs `ntasks` instances of your programm `hostname`
+
+runtime=28800
+max_perturbation=0.001
+
+setopt numeric_glob_sort
+
+if [[ ${${(s./.)PWD}[-1]} != "md.wettbewerb2024" ]]; then {
+    print -Pu2 "%F{red}calc_overview must be run from md.wettbewerb2024 dir.%f"
+    return 1
+}; fi
+
+print -Pu2 "%F{blue}Running all for ${runtime} seconds and with max_perturbation=${max_perturbation}%f"
+
+typeset -A scores=()
+cd -q result_files
+for f in *_score_*.txt(on) ; do {
+    test_case=${${(s._score_.)f}[1]}
+    scores+=( [${test_case}]=${f} )
+}; done
+
+cd -q ..
+typeset -a jobs=()
+for k v in ${(Akv)scores} ; do {
+    jobs+=( "${k}" )
+}; done
+
+srun ./bin/graph_optimization input_files/${jobs[${SLURM_ARRAY_TASK_ID}]}.txt result_files/${scores[${jobs[${SLURM_ARRAY_TASK_ID}]}]} ${runtime} ${max_perturbation}
